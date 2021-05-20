46c2ab1c7bab3b0f38617c61c2caf558f07f540a
unknown <staslukyanenko1995@gmail.com>
2021-05-20 20:02:08 +0300

    start: react-project

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <img src="./images/pokemons.png" alt="log" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
